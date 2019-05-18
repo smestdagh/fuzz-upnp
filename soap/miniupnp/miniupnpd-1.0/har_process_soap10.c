1b4355a1bafd1f47832c1805707a194e8ac2ccad
steven <smestdagh@gmail.com>
2019-05-18 21:55:41 +0200

    add harnesses and seeds for SOAP fuzzing of miniupnpd

---
+++ soap/miniupnp/miniupnpd-1.0/har_process_soap10.c
@@ -0,0 +1,17 @@
+/* test harness for Process_upnphttp function for miniupnp 1.0 */
+
+#include <stdio.h>
+#include "upnphttp.h"
+
+int main() {
+    struct upnphttp * h = NULL;
+    h = New_upnphttp(0);
+    if (h == NULL) {
+	    printf("Allocation error\n");
+	    return -1;
+    }
+    h->state = 0;
+    Process_upnphttp(h);
+    Delete_upnphttp(h);
+    return 0;
+}
